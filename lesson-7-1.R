6f99e2147e812180d1f0939128ef603ca126c22b
Emily <eeise002@fiu.edu>
2016-09-16 10:32:23 -0400

    finished

--- lesson-7-1.R
+++ lesson-7-1.R
@@ -2,16 +2,22 @@
 
 library(rgdal)
 
-counties_md <- readOGR(..., ...)
+counties_md <- readOGR("data/cb_500k_maryland", "cb_500k_maryland")
+## path to folder, layer name
 
+summary(counties_md)
 
 # Basic spatial plots
 
-plot(...)
+plot(counties_md)
 
-howard <- counties_md[..., ]
-plot(howard, ..., ...)
-text(..., ... = counties_md[["NAME"]], cex = ...)
+howard <- counties_md[counties_md[["NAME"]] == "Howard", ]
+## creates a dataframe with one row, a spatial polygon
+plot(howard, add = TRUE, col="red")
+
+text(coordinates(counties_md), 
+     labels = counties_md[["NAME"]], cex = 0.7)
+## cex argument is 70% of size
 
 # Exercise
 
@@ -19,59 +25,80 @@ text(..., ... = counties_md[["NAME"]], cex = ...)
 #  the smallest (1) to largest (24) in land area ("ALAND" attribute). 
 # Hint: Use `rank(x)` to get ranks from a numeric vector x.
 
-...
-
+counties$md
+## unnecessary step
+plot(counties_md)
+text(coordinates(counties_md),
+      labels = rank(counties_md[["ALAND"]]), cex = 0.7)
 
 # Reading rasters into R
 
 library(raster)
 
-nlcd <- raster(...)
+nlcd <- raster("data/nlcd_agg.grd")
 
-plot(...)
+nlcd
+## shows metadata
 
-... <- nlcd@data@attributes[[1]]
+attr_table <- nlcd@data@attributes[[1]]
+
+plot(nlcd)
+plot(counties_md, add = TRUE)
+## won't work becuase not in same projection
 
 
 # Change projections
 
-proj4string(...)
+proj4string(counties_md)
+proj4string(nlcd)
 
-counties_proj <- spTransform(..., proj4string(...))
+counties_proj <- spTransform(counties_md, proj4string(nlcd))
 
 plot(nlcd)
-plot(..., ...)
+plot(counties_proj, add=TRUE)
 
 
 # Masking a raster
 
-pasture <- mask(nlcd, ..., maskvalue = ...)
-plot(...)
+pasture <- mask(nlcd, nlcd == 81, 
+                maskvalue = FALSE)
+## deletes whatever does not equal 81
+
+plot(pasture) 
+## shows only pasture
 
 # Exercise
 
 # Create a mask for a different land cover class. 
 #  Look up the numeric ID for a specific class in attr_table.
 
-...
+high_intensity <- mask(nlcd, nlcd == 24, 
+                maskvalue = FALSE)
+plot(high_intensity)
 
 
 # Adding data to maps with tmap
 
 library(tmap)
 
-qtm(...)
+qtm(counties_proj)
+##similar to plot
 
-qtm(counties_proj, fill = ..., ... = "NAME")
+qtm(counties_proj, fill = "AWATER", 
+    text = "NAME")
 
-map1 <- tm_shape(...) +
-            ...() +
-            ...("AWATER", title = "Water Area (sq. m)") +
-            tm_text(..., size = 0.7)
+map1 <- tm_shape(counties_proj) +
+            tm_borders() +
+            tm_fill("AWATER", title = "Water Area (sq. m)") +
+            tm_text("NAME", size = 0.7)
+## adds layers to new map
+## adds title to legend
+map1
+
 
 map1 +
     tm_style_classic(legend.frame = TRUE) +
-    tm_scale_bar(position = ...)
+    tm_scale_bar(position = c("left", "top"))
 
 
 # Exercise
@@ -80,7 +107,11 @@ map1 +
 # Look at the help file for tm_fill: help("tm_fill") to find which argument
 #  controls the binning scale. How can you change it to a continuous gradient?
 
-...
+map1 <- tm_shape(counties_proj) +
+  tm_borders() +
+  tm_fill("AWATER", title = "Water Area (sq. m)", style = "cont", palette = "PuBu") +
+  tm_text("NAME", size = 0.7)
+map1
 
 
 # Interactive maps with leaflet
@@ -88,11 +119,14 @@ map1 +
 library(leaflet)
 
 imap <- leaflet() %>%
-            ...() %>%
-            ...(lng = -76.505206, lat = 38.9767231, zoom = ...)
+            addTiles() %>%
+            setView(lng = -76.505206, lat = 38.9767231, zoom = 7)
+##creates leaflet object - html
+imap
+
 
 imap %>%
-    ...(
+    addWMSTiles(
         "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",
         layers = "nexrad-n0r-900913", group = "base_reflect",
         options = WMSTileOptions(format = "image/png", transparent = TRUE),
